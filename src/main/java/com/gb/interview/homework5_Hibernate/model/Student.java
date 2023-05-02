56a689d0dbb3f0e93def1412b0cb7c7fd97b5d75
al121 <kb_299@bk.ru>
2023-05-02 22:05:43 +0300

    homework 5 - start

---
+++ src/main/java/com/gb/interview/homework5_Hibernate/model/Student.java
@@ -0,0 +1,18 @@
+package com.gb.interview.homework5_Hibernate.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int mark;
+}
