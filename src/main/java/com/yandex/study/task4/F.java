363e5d8e2129a7c6ac9545d0eb2f0f2efded0894
Alexandr <kb_299@bk.ru>
2023-05-29 10:03:48 +0300

    *

--- src/main/java/com/yandex/study/task4/F.java
+++ src/main/java/com/yandex/study/task4/F.java
@@ -29,6 +29,7 @@ public class F {
                 }
             }
         }
+        reader.close();
 
         for (Map.Entry<String, Map<String, Long>> mapEntry : history.entrySet()) {
             System.out.println(mapEntry.getKey() + ":");
@@ -43,35 +44,33 @@ public class F {
 
 
 
-        Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\GVoichuk\\IdeaProjects\\Test\\src\\main\\java\\com\\yandex\\study\\task4\\F"));
-//        Scanner scanner = new Scanner(new FileInputStream("input.txt"));
+        BufferedReader reader2 = new BufferedReader(new FileReader("C:\\Users\\GVoichuk\\IdeaProjects\\Test\\src\\main\\java\\com\\yandex\\study\\task4\\F"));
+//        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
 
         Map<String, Map<String, Long>> history2 = new TreeMap<>();
-
-        while (scanner.hasNext()){
-            String name = scanner.next();
-            System.out.println("name " + name);
-//            String line = reader.readLine();
-//            String[] s = line.split(" ");
-//            if (!history.containsKey(s[0])){
-//                Map<String, Long> currentMap = new TreeMap<>();
-//                currentMap.put(s[1], (long) Integer.parseInt(s[2]));
-//                history.put(s[0], currentMap);
-//            } else {
-//                if (!history.get(s[0]).containsKey(s[1])){
-//                    history.get(s[0]).put(s[1], (long) Integer.parseInt(s[2]));
-//                } else {
-//                    history.get(s[0]).replace(s[1], history.get(s[0]).get(s[1]) + (long) Integer.parseInt(s[2]));
-//                }
-//            }
+        while (reader2.ready()){
+            String line = reader2.readLine();
+            String[] s = line.split(" ");
+            if (!history2.containsKey(s[0])){
+                Map<String, Long> currentMap = new TreeMap<>();
+                currentMap.put(s[1], (long) Integer.parseInt(s[2]));
+                history2.put(s[0], currentMap);
+            } else {
+                if (!history2.get(s[0]).containsKey(s[1])){
+                    history2.get(s[0]).put(s[1], (long) Integer.parseInt(s[2]));
+                } else {
+                    history2.get(s[0]).replace(s[1], history2.get(s[0]).get(s[1]) + (long) Integer.parseInt(s[2]));
+                }
+            }
         }
+        reader2.close();
 
-//        for (Map.Entry<String, Map<String, Long>> mapEntry : history.entrySet()) {
-//            System.out.println(mapEntry.getKey() + ":");
-//            for (Map.Entry<String, Long> longEntry : mapEntry.getValue().entrySet()) {
-//                System.out.println(longEntry.getKey() + " " + longEntry.getValue());
-//            }
-//        }
+        for (Map.Entry<String, Map<String, Long>> mapEntry : history2.entrySet()) {
+            System.out.println(mapEntry.getKey() + ":");
+            for (Map.Entry<String, Long> longEntry : mapEntry.getValue().entrySet()) {
+                System.out.println(longEntry.getKey() + " " + longEntry.getValue());
+            }
+        }
 
 
 
